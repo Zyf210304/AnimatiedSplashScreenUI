500ac69bf1c28aac9d4bf167cfe1bc296d54d1be
张亚飞 <672233580@qq.com>
2021-06-26 19:40:46 +0800

    Initial Commit

---
+++ AnimatiedSplashScreenUI/AnimatiedSplashScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatiedSplashScreenUIApp.swift
+//  AnimatiedSplashScreenUI
+//
+//  Created by 张亚飞 on 2021/6/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimatiedSplashScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
